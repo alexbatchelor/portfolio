c5d012ed8e02884972c307159ffabe569759d6cb
alexbatchelor <43844799+alexbatchelor@users.noreply.github.com>
2024-03-21 15:38:03 +0000

    up

--- navigation.js
+++ navigation.js
@@ -1,21 +1,15 @@
 // Load the projects.json file
 fetch('projects.json')
-  .then(response => {
-    console.log('Fetch response:', response);
-    return response.json();
-  })
+  .then(response => response.json())
   .then(projects => {
-    console.log('Projects:', projects);
     // Get the current project path
-    const currentPath = window.location.pathname;
-    console.log('Current path:', currentPath);
+    const currentPath = window.location.pathname.replace(/\/$/, ''); // Remove trailing slash
 
     // Normalize function to remove leading and trailing slashes
     const normalizeUrl = url => url.replace(/^\/+|\/+$/g, '');
 
     // Find the current project in the array
     const currentProjectIndex = projects.findIndex(project => normalizeUrl(project.url) === normalizeUrl(currentPath));
-    console.log('Current project index:', currentProjectIndex);
 
     // Set up the previous and next project links
     const previousProject = projects[currentProjectIndex - 1];
@@ -23,14 +17,9 @@ fetch('projects.json')
 
     // Update the previous and next project links
     if (previousProject) {
-      console.log('Previous project:', previousProject);
       document.getElementById('previous-link').href = previousProject.url;
     }
     if (nextProject) {
-      console.log('Next project:', nextProject);
       document.getElementById('next-link').href = nextProject.url;
     }
-  })
-  .catch(error => {
-    console.error('Fetch error:', error);
   });
